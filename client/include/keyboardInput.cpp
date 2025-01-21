e9a08e39b3ae5655387e06eaa03f4f4777fd7b01
noakf <noa.kfir2@gmail.com>
2025-01-21 15:57:55 +0000

    created keyboardInput

---
+++ client/include/keyboardInput.cpp
@@ -0,0 +1,18 @@
+#include "keyboardInput.h"
+
+void split_str(string line, char sign, vector<string> lineArgs)
+{
+    int i = 0;
+    string args = "";
+    for (int j = 0; j < line.length(); j++)
+    {
+        if(line.at(j) == sign)
+        {
+            lineArgs.at(i) = args;
+            i += 1;
+        }
+        else
+        args += line.at(j);
+    }
+    lineArgs.at(i) = args;
+}
