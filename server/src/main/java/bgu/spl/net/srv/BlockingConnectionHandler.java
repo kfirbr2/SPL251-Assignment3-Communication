fbef255e5624a48666c29d921d9d1c93c01ef26f
kfirbr2 <kfirbr2@gmail.com>
2025-01-26 15:19:56 +0200

    Update BlockingConnectionHandler.java

--- server/src/main/java/bgu/spl/net/srv/BlockingConnectionHandler.java
+++ server/src/main/java/bgu/spl/net/srv/BlockingConnectionHandler.java
@@ -1,7 +1,6 @@
 package bgu.spl.net.srv;
-
-import bgu.spl.net.api.MessageEncoderDecoder;
-import bgu.spl.net.api.MessagingProtocol;
+import bgu.spl.net.impl.stomp.*;
+import bgu.spl.net.api.StompMessagingProtocol;
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.IOException;
@@ -9,17 +8,18 @@ import java.net.Socket;
 
 public class BlockingConnectionHandler<T> implements Runnable, ConnectionHandler<T> {
 
-    private final MessagingProtocol<T> protocol;
-    private final MessageEncoderDecoder<T> encdec;
+    private final StompMessagingProtocol<T> protocol;
+    private final StompMessageEncoderDecoder<T> encdec;
     private final Socket sock;
     private BufferedInputStream in;
     private BufferedOutputStream out;
     private volatile boolean connected = true;
 
-    public BlockingConnectionHandler(Socket sock, MessageEncoderDecoder<T> reader, MessagingProtocol<T> protocol) {
+    public BlockingConnectionHandler(Socket sock, StompMessageEncoderDecoder<T> reader, StompMessagingProtocol<T> protocol , int connectionId) {
         this.sock = sock;
         this.encdec = reader;
         this.protocol = protocol;
+        this.protocol.start(connectionId,(ConnectionImpl)ConnectionImpl.getInstance());
     }
 
     @Override
@@ -33,13 +33,11 @@ public class BlockingConnectionHandler<T> implements Runnable, ConnectionHandler
             while (!protocol.shouldTerminate() && connected && (read = in.read()) >= 0) {
                 T nextMessage = encdec.decodeNextByte((byte) read);
                 if (nextMessage != null) {
-                    T response = protocol.process(nextMessage);
-                    if (response != null) {
-                        out.write(encdec.encode(response));
-                        out.flush();
+                     protocol.process(nextMessage);
+                     
                     }
                 }
-            }
+           
 
         } catch (IOException ex) {
             ex.printStackTrace();
