c880bd661399b169b4638167138ab70d18677bbd
kfirbr2 <kfirbr2@gmail.com>
2025-01-26 15:21:19 +0200

    Update BaseServer.java

--- server/src/main/java/bgu/spl/net/srv/BaseServer.java
+++ server/src/main/java/bgu/spl/net/srv/BaseServer.java
@@ -5,6 +5,7 @@ import bgu.spl.net.impl.stomp.*;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Supplier;
 
 public abstract class BaseServer<T> implements Server<T> {
@@ -13,7 +14,7 @@ public abstract class BaseServer<T> implements Server<T> {
     private final Supplier<StompMessagingProtocol<T>> protocolFactory;
     private final Supplier<StompMessageEncoderDecoder<T>> encdecFactory;
     private ServerSocket sock;
-
+    private final AtomicInteger connectionId = new AtomicInteger(1);
     public BaseServer(
             int port,
             Supplier<StompMessagingProtocol<T>> protocolFactory,
@@ -23,24 +24,30 @@ public abstract class BaseServer<T> implements Server<T> {
         this.protocolFactory = protocolFactory;
         this.encdecFactory = encdecFactory;
 		this.sock = null;
+        
     }
+      
+   
 
     @Override
     public void serve() {
 
         try (ServerSocket serverSock = new ServerSocket(port)) {
-			System.out.println("Server started");
+			System.out.println("Server started ");
 
             this.sock = serverSock; //just to be able to close
 
             while (!Thread.currentThread().isInterrupted()) {
 
                 Socket clientSock = serverSock.accept();
-
+                int currentId = connectionId.getAndIncrement();
                 BlockingConnectionHandler<T> handler = new BlockingConnectionHandler<>(
                         clientSock,
                         encdecFactory.get(),
-                        protocolFactory.get());
+                        protocolFactory.get(),currentId);
+                        ((ConnectionImpl)(ConnectionImpl.getInstance())).register(handler,currentId);
+                        
+
 
                 execute(handler);
             }
