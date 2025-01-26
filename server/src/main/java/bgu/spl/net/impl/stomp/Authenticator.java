a6884a2e25ef2658909832108207f965726a22a0
kfirbr2 <kfirbr2@gmail.com>
2025-01-26 15:22:08 +0200

    Update Authenticator.java

--- server/src/main/java/bgu/spl/net/impl/stomp/Authenticator.java
+++ server/src/main/java/bgu/spl/net/impl/stomp/Authenticator.java
@@ -1,17 +1,21 @@
 package bgu.spl.net.impl.stomp;
-
-import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class Authenticator {
     private static final Map<String , String> users = new ConcurrentHashMap<>();
     private static final Map<Integer , String> activesUsers = new ConcurrentHashMap<>();
+    
 
     public static synchronized String authenticate( String login, String passcode, int connectionId) {
-        if(activesUsers.containsKey(connectionId)) {
-            return "ALREADY_LOGGED_IN";
+        boolean found=false;
+        for (String value : activesUsers.values()){
+            if(value.equals(login)){
+               found=true;}
+        }
 
+        if( found){
+            return "ALREADY_LOGGED_IN";
         }
         if(users.containsKey(login) ) {
             if(users.get(login).equals(passcode)) {
